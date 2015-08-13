b9ea97c240c7b9fb51eec65e5f56de75216d051b
Harish Udaiya Kumar <harishspqr@gmail.com>
2015-08-13 13:43:01 -0700

    Renaming the gpu drivers package name in script

--- common/data/update_packages_minimal.sh
+++ common/data/update_packages_minimal.sh
@@ -2,5 +2,5 @@
 
 cp -L packages_minimal.json packages_minimal2.json
 sed -i 's/photon-release/vivace-release/g;s/open-vm-tools/open-vm-tools-vivace/g' packages_minimal2.json
-sed -i 's/]/, "linux-gpu-drivers"]/g' packages_minimal2.json
+sed -i 's/]/, "linux-drivers-gpu"]/g' packages_minimal2.json
 
